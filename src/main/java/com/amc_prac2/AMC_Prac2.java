eeb07ee10def7ca30ea1caf2a123a9e4a744ec57
javiiariass <javierariasfuentes@gmail.com>
2024-12-03 13:27:00 +0100

    Añadido proyecto Maven

---
+++ src/main/java/com/amc_prac2/AMC_Prac2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amc_prac2;
+
+/**
+ *
+ * @author javie
+ */
+public class AMC_Prac2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
